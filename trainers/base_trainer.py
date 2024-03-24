d4e735cffab069dab0bc169d7fb47065a8f4b4ee
mehulcse <mehulthakkar02@gmail.com>
2024-03-24 13:05:34 +0530

    Updated Trainer flow

--- trainers/base_trainer.py
+++ trainers/base_trainer.py
@@ -30,7 +30,7 @@ class BaseTrainer:
 
         self.train_loader, self.val_loader = self._init_dataloaders()
 
-        pretrained_path = self.config.get("model_path", False)
+        pretrained_path = self.config.get("model_path", True)
         if pretrained_path:
             self.training_epoch, self.total_iter = load_checkpoint(
                 pretrained_path, self.model, optimizer=self.optimizer,
